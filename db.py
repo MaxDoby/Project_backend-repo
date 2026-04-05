a1cd90ffb23c7f210a20155f96e14029ae5ffe25
Maxim Dobinda <maxim.benid@gmail.com>
2026-04-05 12:14:25 +0300

    add create commands

---
+++ db.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'root',
+    database = 'backend_database'
+)
+
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(25) NOT NULL
+)
+""")
+
+cursor.execute("""
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
+    title VARCHAR(255),
+    status VARCHAR(20),
+    FOREIGN KEY(student_id) REFERENCES students(id)
+)
+""")
+
+connection.commit()
